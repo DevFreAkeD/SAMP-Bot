6a5c249720e62637ab1f386f78f0125d8ef48d3f
Tushar Nagar <tusharrock146@gmail.com>
2024-07-01 11:27:30 +0530

    Update commands.js
    !ip command added

--- utils/commands.js
+++ utils/commands.js
@@ -9,4 +9,39 @@ module.exports = [
             await message.reply('Pong!');
         }
     },
+    {
+        name: 'ip',
+        aliases: [],
+        description: 'Shows IP address of a SA:MP Server',
+        async execute(client, message, args) {
+            if(!process.env.SAMP_SERVER_IP)
+                return message.channel.send('IP address is not set in the .env file!');
+
+            const ip = process.env.SAMP_SERVER_IP.split(':');
+            const options = {
+                host: ip[0],
+                port: ip[1] || 7777
+            };
+
+            const embed = new EmbedBuilder();
+            const color = await message.guild?.members.fetch(message.client.user.id).then(color => color.displayHexColor) || '#000000';
+            
+            await samp(options, (error, query) => {
+                if(error){
+                    embed.setColor(color);
+                    embed.setTitle('Server is offline');
+                    embed.setDescription(`**IP:** \`${options.host}:${options.port}\``);
+                    return message.channel.send({ embeds: [embed] });
+                }
+                else{
+                    embed.setColor(color);
+                    embed.setTitle('Server is online!');
+                    embed.setDescription(`**IP:** \`${options.host}:${options.port}\``);
+                    return message.channel.send({ embeds: [embed] });
+                }
+            });
+
+            return;
+        }
+    }
 ];
